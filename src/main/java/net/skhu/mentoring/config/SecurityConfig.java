6e65c56f045d6cb26d30d6eaff8800c62cd2271c
Kang In Sung <tails5555@naver.com>
2017-10-31 20:10:08 +0900

    little error...ㅜㅜ

---
+++ src/main/java/net/skhu/mentoring/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package net.skhu.mentoring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import net.skhu.mentoring.service.MyAuthenticationProvider;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Autowired MyAuthenticationProvider myAuthenticationProvider;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        web.ignoring().antMatchers("/res/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+            .antMatchers("/employee/**").access("ROLE_EMPLLOYEE")
+            .antMatchers("/professor/**").access("ROLE_PROFESSOR")
+            .antMatchers("/studChairman/**").access("ROLE_STUDCHAIRMAN")
+            .antMatchers("/mento/**").access("ROLE_MENTO")
+            .antMatchers("/menti/**").access("ROLE_MENTI")
+            .antMatchers("/guest/**").permitAll()
+            .antMatchers("/").permitAll()
+            .antMatchers("/**").authenticated();
+
+        http.csrf().disable();
+
+        http.formLogin()
+            .loginPage("/guest/login")
+            .loginProcessingUrl("/guest/login_processing")
+            .failureUrl("/guest/login?error")
+            .defaultSuccessUrl("/user/index", true)
+            .usernameParameter("loginId")
+            .passwordParameter("passwd");
+
+        http.logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout_processing"))
+            .logoutSuccessUrl("/guest/login")
+            .invalidateHttpSession(true);
+
+        http.authenticationProvider(myAuthenticationProvider);
+    }
+}
